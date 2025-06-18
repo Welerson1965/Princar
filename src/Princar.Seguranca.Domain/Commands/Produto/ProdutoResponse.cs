2c404bd9a53b332c96726ce70d9f1a20d40ea466
Fernando Silva <fernando@ispac.com.br>
2025-06-18 13:48:00 -0300

    Acerto

--- src/Princar.Seguranca.Domain/Commands/Produto/ProdutoResponse.cs
+++ src/Princar.Seguranca.Domain/Commands/Produto/ProdutoResponse.cs
@@ -5,5 +5,6 @@
         public string Descricao { get; set; }
         public decimal Preco { get; set; }
         public decimal Quantidade { get; set; }
+        public int CodigoProduto { get; set; }
     }
 }
--- src/Princar.Seguranca.Domain/Entities/Produto/ProdutoEntity.cs
+++ src/Princar.Seguranca.Domain/Entities/Produto/ProdutoEntity.cs
@@ -8,15 +8,17 @@ namespace Princar.Seguranca.Domain.Entities.Produto
         public string Descricao { get; private set; }
         public decimal Preco { get; private set; }
         public decimal Quantidade { get; private set; }
+        public int CodigoProduto { get; private set; }
 
         //EF
         public ProdutoEntity() { }
 
-        public ProdutoEntity(string descricao, decimal preco, decimal quantidade)
+        public ProdutoEntity(string descricao, decimal preco, decimal quantidade, int codigoProduto)
         {
             Descricao = descricao;
             Preco = preco;
             Quantidade = quantidade;
+            CodigoProduto = codigoProduto;
         }
     }
 }
