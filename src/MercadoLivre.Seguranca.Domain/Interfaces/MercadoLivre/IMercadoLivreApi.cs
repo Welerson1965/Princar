5ef0662a4e3916d744dd312c7987c06d800ed14c
Fernando Silva <fernando@ispac.com.br>
2025-08-07 08:31:48 -0300

    acertos

--- src/MercadoLivre.Seguranca.Domain/Interfaces/MercadoLivre/IMercadoLivreApi.cs
+++ src/MercadoLivre.Seguranca.Domain/Interfaces/MercadoLivre/IMercadoLivreApi.cs
@@ -6,7 +6,6 @@ namespace MercadoLivre.Seguranca.Domain.Interfaces.MercadoLivre
     public interface IMercadoLivreApi
     {
         TokenRetornoDTO BuscarToken(Guid empresaId);
-
         PedidoIndividualRetornoDTO BuscarPedidoIndividual(string pedidoId, string token);
         PedidoEnvioRetornoDTO BuscarPedidoEnvio(string envioId, string token);
     }
